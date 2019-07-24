90bde3610ad23ed896a57aa21ad2a542ac5600d1
18845126752 <2634440521@qq.com>
2019-07-24 15:08:33 +0800

    lianxi

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,26 +1,41 @@
-const path = require("path");0
-module.exports={
-    entry:{
-        index:"./src/index.js"
-    },
-    output:{
-        path:path.resolve(__dirname,"dist"),
-        filename:'[name].js',
-    },
-    // plugins:[
-    //     new HtmlWebpackPlugin({
-    //         minify:{
-    //             removeArrtibuteQuotes:true
-    //         },
-    //         hash:true,
-    //         filename:'index.html',
-    //         template:'src/index.html',
-    //     })
-    // ],
-    // devServer:{
-    //     contentBase:path.resolve(__dirname,"dist"),
-    //     host:"localhost",
-    //     compress:true,
-    //     port:"8081",
-    // }
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+module.exports = {
+  entry:{
+    index:"./src/index.js"
+  },
+  output:{
+    path:path.resolve(__dirname,"dist"),
+    filename:'[name].js'
+  },
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback:"style-loader",
+          use:"css-loader"
+        })
+      }
+    ]
+  },
+
+  plugins:[
+    new ExtractTextPlugin("css/main.css"),
+    new HtmlWebpackPlugin({  
+      minify:{
+        removeAttributeQuotes:true
+      },
+      hash:true,
+      filename: 'index.html',
+      template: 'src/index.html'
+    })
+  ],
+  devServer:{
+    contentBase:path.resolve(__dirname,"dist"),
+    host:"localhost",
+    compress:true,
+    port:"8081"
+  }
 }
